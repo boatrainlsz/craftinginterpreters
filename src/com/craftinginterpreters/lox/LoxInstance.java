5cb4826b0af547cd7ae392154c14e4137fc548f2
boatrainlsz <yinger521999@gmail.com>
2022-10-16 10:25:45 +0800

    12.5完成

--- src/com/craftinginterpreters/lox/LoxInstance.java
+++ src/com/craftinginterpreters/lox/LoxInstance.java
@@ -15,11 +15,15 @@ class LoxInstance {
     void set(Token name, Object value) {
         fields.put(name.lexeme, value);
     }
+
     Object get(Token name) {
         if (fields.containsKey(name.lexeme)) {
             return fields.get(name.lexeme);
         }
-
+        LoxFunction method = klass.findMethod(name.lexeme);
+        if (method != null) {
+            return method;
+        }
         throw new RuntimeError(name,
                 "Undefined property '" + name.lexeme + "'.");
     }
--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -26,6 +26,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitClassStmt(Stmt.Class stmt) {
         declare(stmt.name);
         define(stmt.name);
+        for (Stmt.Function method : stmt.methods) {
+            FunctionType declaration = FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
         return null;
     }
 
@@ -227,6 +231,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     private enum FunctionType {
-        NONE, FUNCTION
+        NONE, FUNCTION, METHOD
+
     }
 }
