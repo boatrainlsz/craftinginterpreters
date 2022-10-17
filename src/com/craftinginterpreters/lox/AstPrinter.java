05dc480f9bace3c03548ee16cac6a5277470ba11
boatrainlsz <yinger521999@gmail.com>
2022-10-17 20:07:18 +0800

    13.2完成

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -44,6 +44,11 @@ class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitThisExpr(Expr.This expr) {
+        return null;
+    }
+
+    @Override
     public String visitGroupingExpr(Expr.Grouping expr) {
         return parenthesize("group", expr.expression);
     }
